db9402f287f64481db1be9ba5c1bec6abe39a40e
jeikobbl <jeikobjga@gmail.com>
2022-08-25 17:50:41 -0600

    Tipo de variable id

--- SuperHeroAPI/Superhero.cs
+++ SuperHeroAPI/Superhero.cs
@@ -1,7 +1,7 @@
 ﻿namespace SuperHeroAPI
 {
     public class Superhero {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; } 
