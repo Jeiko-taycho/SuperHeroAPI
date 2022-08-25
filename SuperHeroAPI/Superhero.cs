432f3b1eeafae0431154208abb47292a574dd381
jeikobbl <jeikobjga@gmail.com>
2022-08-25 15:54:20 -0600

    Agregar clase Superhero

---
+++ SuperHeroAPI/Superhero.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperHeroAPI
+{
+    public class Superhero {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; } 
+        public string Place { get; set; }
+    }
+}
